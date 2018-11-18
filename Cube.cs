d4b18d5195f198070288bd3bcf8692e3454f9b46
Drews-Bshark <44979108+Drews-Bshark@users.noreply.github.com>
2018-11-18 22:09:22 +0300

    Add files via upload

---
+++ Cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Cube : MonoBehaviour
+{
+    private float speed;
+    public GameObject objects;  
+    private void Start()
+    {
+        speed = Random.Range(0.05F, 0.1F);
+    }
+    void Update()
+    {
+        objects.transform.Translate(0, -speed, 0);
+        if (transform.position.x <= -6F && (Input.GetKeyDown("a") && transform.position.y <= -3F))
+        {
+            Debug.Log("Precision:" + (4.5 - transform.position.y) * 10);
+            GameObject.Destroy(objects);
+        }
+        if (transform.position.x > -6F && transform.position.x < 6F && Input.GetKeyDown("s") && transform.position.y <= -3F)
+        {
+            Debug.Log("Precision:" + (4.5 - transform.position.y) * 10);
+            GameObject.Destroy(objects);
+        }
+        if (transform.position.x >= 6F && Input.GetKeyDown("d") && transform.position.y <= -3F)
+        {
+            Debug.Log("Precision:" + (4.5 - transform.position.y) * 10);
+            GameObject.Destroy(objects);
+        }
+        if(transform.position.y <= -4.5F)
+            GameObject.Destroy(objects);
+
+    }      
+}
---
+++ CubeSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject objects;
+    public float x = 100F;
+    private float i = 0F;
+    private bool flag = false;
+
+    void Update()
+    {
+        float speed = Random.Range(0.0001F, 0.001F);
+        i += 0.8F;
+        int rand = Random.Range(0, 3);
+
+
+        if (i > x)
+        {
+            if (rand == 0)
+            {
+                GameObject.Instantiate(objects);
+                objects.transform.position = new Vector3(-8F, 4F);
+            }
+            if (rand == 1)
+            {
+                GameObject.Instantiate(objects);
+                objects.transform.position = new Vector3(0F, 4F);
+            }
+            if (rand == 2)
+            {
+                GameObject.Instantiate(objects);
+                objects.transform.position = new Vector3(8F, 4F);
+            }
+            i = 0;            
+        }
+    }
+}
+
+
